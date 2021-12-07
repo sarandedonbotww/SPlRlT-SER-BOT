b32f63fec97292add434fc445c791548bba85248
sarandedonbotww <94845207+sarandedonbotww@users.noreply.github.com>
2021-12-07 09:11:10 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -39,7 +39,7 @@ var base = `https://gist.github.com/`
 var PROP = { aredits: '919946432377,0' }
 var unlink = `019112af334adceaefd1467dcbd93e58` 
 var PROP2 = { kl11: '919074309534,0' }
-var string = base + `souravkl11`
+var string = base + `SPlRlT
         
 // Yalnızca bir kolaylık. https://stackoverflow.com/questions/4974238/javascript-equivalent-of-pythons-format-function //
 String.prototype.format = function () {
